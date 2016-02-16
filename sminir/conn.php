b2527a4adf18d8e6034c5b92b4674e9c49a69e61
Daniel Teodoro <i.am.daniel08@gmail.com>
2016-02-16 21:29:31 +0800

    Update conn.php

--- sminir/conn.php
+++ sminir/conn.php
@@ -1,3 +1,3 @@
 <?php
-	$con = mysqli_connect("localhost","adminGyYDuRe","9UXnqTq53CGD","sminir") or die("Could not connect.".mysql_error());
+	$con = mysqli_connect("127.3.59.2:3306","adminGyYDuRe","9UXnqTq53CGD","sminir") or die("Could not connect.".mysql_error());
 ?>
